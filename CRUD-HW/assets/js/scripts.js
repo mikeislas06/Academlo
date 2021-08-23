a26d806401243933cf918fa5904318fbc122e3ea
Miguel Islas <isgromiguel@gmail.com>
2021-08-23 18:30:43 -0500

    User info input creation

--- CRUD-HW/assets/js/scripts.js
+++ CRUD-HW/assets/js/scripts.js
@@ -75,9 +75,77 @@ const users = [
 
 //Crear una tabla con cualquier técnica (flex, grid, tables, divs) que muestre cada objeto del arreglo users
 
+const createUser = (e) => {
+  // No ejecute su comportamiento por defecto
+  e.preventDefault();
+
+  //Verificar Redes sociales seleccionadas
+  let socialMedia = [];
+  if (document.getElementById("Facebook").checked) {
+    const social = {
+      nombre: "Facebook",
+      url: "facebook.com/" + document.getElementById("name").value,
+    };
+    socialMedia.push(social);
+  }
+  if (document.getElementById("Youtube").checked) {
+    const social = {
+      nombre: "Youtube",
+      url: "youtube.com/" + document.getElementById("name").value,
+    };
+    socialMedia.push(social);
+  }
+  if (document.getElementById("Twitter").checked) {
+    const social = {
+      nombre: "Twitter",
+      url: "twitter.com/" + document.getElementById("name").value,
+    };
+    socialMedia.push(social);
+  }
+  if (document.getElementById("Instagram").checked) {
+    const social = {
+      nombre: "Instagram",
+      url: "instagram.com/" + document.getElementById("name").value,
+    };
+    socialMedia.push(social);
+  }
+  if (document.getElementById("Github").checked) {
+    const social = {
+      nombre: "Github",
+      url: "github.com/" + document.getElementById("name").value,
+    };
+    socialMedia.push(social);
+  }
+
+  // Crear un objeto con la informacion de los inputs
+  let user = {
+    nombre: document.getElementById("name").value,
+    correo: document.getElementById("email").value,
+    pais: document.getElementById("country").value,
+    social: socialMedia,
+  };
+
+  // Agregamos el objeto al arreglo
+  users.push(user);
+  renderList();
+
+  //Limpiar campos
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("country").value = "";
+  document.getElementById("Facebook").checked = false;
+  document.getElementById("Youtube").checked = false;
+  document.getElementById("Twitter").checked = false;
+  document.getElementById("Instagram").checked = false;
+  document.getElementById("Github").checked = false;
+
+  console.log(users);
+};
+
 const tbody = document.getElementById("tbody");
 
 const renderList = () => {
+  tbody.innerHTML = "";
   if (users.length > 0) {
     users.forEach((user) => {
       const userRow = document.createElement("tr");
@@ -128,4 +196,9 @@ const renderList = () => {
   }
 };
 
+const form = document.getElementById("main-form");
+
+// Eventos
 renderList();
+form.onsubmit = createUser;
+console.log(users);
