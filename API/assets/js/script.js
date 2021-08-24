9f5d5e1cefbab5c49bad86993234b1a2dcefe404
Miguel Islas <isgromiguel@gmail.com>
2021-08-23 21:08:21 -0500

    fetch promesa result

--- API/assets/js/script.js
+++ API/assets/js/script.js
@@ -3,7 +3,9 @@ const API = "https://pokeapi.co/api/v2/";
 const button = document.getElementById("searchPokemon");
 
 const handleSearchPokemon = () => {
-  fetch(`${API}pokemon?limit=20&offset=1`);
+  fetch(`${API}pokemon?limit=20&offset=0`).then((response) =>
+    response.json().then((result) => console.log(result))
+  );
   //   console.log(`${API}pokemon?limit=20&offset=1`);
 };
 
