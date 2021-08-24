20785cdec329377b933763977d7703eb496407d7
Miguel Islas <isgromiguel@gmail.com>
2021-08-23 21:44:26 -0500

    trivia API checked

--- API/assets/js/script.js
+++ API/assets/js/script.js
@@ -1,12 +1,25 @@
 const API = "https://pokeapi.co/api/v2/";
 
 const button = document.getElementById("searchPokemon");
+const triviaButton = document.getElementById("triviaBtn");
 
 const handleSearchPokemon = () => {
-  fetch(`${API}pokemon?limit=20&offset=0`).then((response) =>
-    response.json().then((result) => console.log(result))
+  fetch(`${API}pokemon/charizard`).then((response) =>
+    response
+      .json()
+      .then((result) => console.log(result.sprites.front_default))
+      .catch((err) => console.log(err))
   );
-  //   console.log(`${API}pokemon?limit=20&offset=1`);
+};
+
+const handleRespTrivia = () => {
+  fetch("https://opentdb.com/api.php?amount=10").then((response) =>
+    response
+      .json()
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err))
+  );
 };
 
 button.onclick = handleSearchPokemon;
+triviaButton.onclick = handleRespTrivia;
