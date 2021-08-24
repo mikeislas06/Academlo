040498d062049ffa7aab7f8899adf89a5c25af1c
Miguel Islas <isgromiguel@gmail.com>
2021-08-23 20:53:52 -0500

    fetch

---
+++ API/assets/js/script.js
@@ -0,0 +1,10 @@
+const API = "https://pokeapi.co/api/v2/";
+
+const button = document.getElementById("searchPokemon");
+
+const handleSearchPokemon = () => {
+  fetch(`${API}pokemon?limit=20&offset=1`);
+  //   console.log(`${API}pokemon?limit=20&offset=1`);
+};
+
+button.onclick = handleSearchPokemon;
