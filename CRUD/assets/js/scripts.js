4e62cb39dc815bd2f796722ec7e659618d9ad534
Miguel Islas <isgromiguel@gmail.com>
2021-08-23 20:05:46 -0500

    Delete and Edit buttons functions created

--- CRUD/assets/js/scripts.js
+++ CRUD/assets/js/scripts.js
@@ -1,9 +1,23 @@
 const form = document.getElementById("main-form");
 const usersContainer = document.getElementById("usersContainer");
+const modalContent = document.getElementById("modalContent");
 
 // Variables útlies
 
-const users = [];
+const users = [
+  {
+    nombre: "Daniel",
+    ocupacion: "Front End",
+  },
+  {
+    nombre: "Miguel",
+    ocupacion: "Front End",
+  },
+  {
+    nombre: "Jorge",
+    ocupacion: "UX/UI",
+  },
+];
 
 // Funciones
 
@@ -21,14 +35,13 @@ const createUser = (e) => {
   // Agregamos el objeto al arreglo
   users.push(user);
   renderList();
-  console.log(users);
 };
 
 //Función encargada de mostrar todos los usuarios
 const renderList = () => {
   usersContainer.innerHTML = "";
   if (users.length > 0) {
-    users.forEach((user) => {
+    users.forEach((user, index) => {
       //Creamos el contenedor principal para el usuario
       const userContainer = document.createElement("div");
       userContainer.classList.add("userItem");
@@ -52,8 +65,16 @@ const renderList = () => {
 
       updateButton.innerText = "Editar";
       updateButton.classList.add("update");
+      updateButton.id = "edit" + index;
       deleteButton.innerText = "Eliminar";
       deleteButton.classList.add("delete");
+
+      //Eventos a los botones
+      //Eliminar
+      deleteButton.onclick = () => deleteUser(index);
+
+      //Editar
+      updateButton.onclick = () => editUser(index);
 
       //Agregar al respectivo container
       usersContainer.appendChild(userContainer);
@@ -67,6 +88,42 @@ const renderList = () => {
   }
 };
 
+//Funcion para eliminar registros
+const deleteUser = (index) => {
+  users.splice(index, 1);
+  renderList();
+};
+
+//Funcion para editar registro
+const editUser = (index) => {
+  console.log(users[index]);
+  modal.style.display = "block";
+  const inputName = document.createElement("input");
+  const labelName = document.createElement("label");
+  const inputJob = document.createElement("input");
+  const labelJob = document.createElement("label");
+};
+
 // Eventos
+document.addEventListener("DOMContentLoaded", renderList);
 form.onsubmit = createUser;
 console.log(users);
+
+//Modal script
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
